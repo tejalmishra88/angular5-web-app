a8b808a321e8a3ea2d9b460412d1a6108486949c
tejalmishra88 <tejalmishra88@gmail.com>
2018-04-07 15:31:55 +1000

    ngStyle changed

--- src/app/loremipsum/loremipsum.component.ts
+++ src/app/loremipsum/loremipsum.component.ts
@@ -9,7 +9,15 @@ export class LoremipsumComponent implements OnInit {
 name = "tejal mishra";
 cls = 'colorgreen';
 cls2 = 'colorred';
+cls3 = 'colorblue';
 defaultvalue = "";
+/*colors = [
+  this.cls, this.cls2, this.cls3,
+]*/
+colors = {
+      ' colorgreen colorblue colorred':true,
+}
+inlineColor = 'red';
 defaultplaceholder="default placeholder";
 times = 0;
 show: boolean = true;
